ae47e8395121828d99634f07e401e0fd94bfb158
TaylanSoydan <taylansoydan@gmail.com>
2023-05-01 20:38:17 +0200

    8

--- exercise_session_08/assign.cxx
+++ exercise_session_08/assign.cxx
@@ -280,10 +280,11 @@ std::cout<< "starting to find slab cut indexes" << "\n";
 for (int i = i_start; i < i_end; ++i){
     int particle_index = int(floor((r(i, 0) + 0.5))*nGrid);
     int next_particle_index = int(floor((r(i+1, 0) + 0.5))*nGrid);
-    
+    std::cout << "particle_index = " << particle_index << " " << "next particle index = " << next_particle_index << "\n";
+    std::cout << "cut_point = " << i+1-i_start << "\n";
     if (next_particle_index > particle_index) {
-        std::cout << "particle_index = " << particle_index << " " << "next particle index = " << next_particle_index << "\n";
-        std::cout << "cut_point = " <<i+1-i_start << "\n";
+        
+        
         //assert (counter < int(nGrid / N_rank));
         slab_cut_indexes[counter] = i+1-i_start;
         counter++;
