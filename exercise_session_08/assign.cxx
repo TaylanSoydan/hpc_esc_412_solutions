3ef9c5071f2991501bd19b0e521fe8dac00225f7
TaylanSoydan <taylansoydan@gmail.com>
2023-05-03 18:20:03 +0200

    z

--- exercise_session_08/assign.cxx
+++ exercise_session_08/assign.cxx
@@ -332,7 +332,7 @@ int main(int argc, char *argv[]){
     delete [] num_particles_to_send;
     delete [] num_particles_to_recv;
     blitz::Array<float, 2> rsorted(r_sorted, blitz::shape(total_num_particles_to_recv,3), blitz::deleteDataWhenDone);
-    printf("Shape of r: (%ld, %ld)\n", rsorted.shape()[0], rsorted.shape()[1]);
+    printf("Shape of r: (%d, %d)\n", rsorted.shape()[0], rsorted.shape()[1]);
     printf("Start index of r: %d\n", rsorted.lbound());
     printf("End index of r: %d\n", rsorted.ubound());
     printf("Sum of r: %f\n", blitz::sum(rsorted));
