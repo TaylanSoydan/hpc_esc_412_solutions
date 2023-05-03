a2922daf08a386d348edf44aec2cf52731d1b98a
TaylanSoydan <taylansoydan@gmail.com>
2023-05-03 18:04:36 +0200

    8

--- exercise_session_08/assign.cxx
+++ exercise_session_08/assign.cxx
@@ -354,37 +354,37 @@ int main(int argc, char *argv[]){
     //    upperboundary = (float) upperbound / nGrid;
     //    printf("i_rank = %d upperboundary = %f\n", i_rank, upperboundary);    
     //    printf("i_start = %d i_start + total_num_particles_to_recv = %d\n", i_start, i_start + total_num_particles_to_recv);    
-    //    #pragma omp parallel for
-    //    for (int pn = i_start; pn < i_start + total_num_particles_to_recv ; ++pn)
-    //    {
-    //        float x = rsorted(pn, 0);
-    //        float y = rsorted(pn, 1);
-    //        float z = rsorted(pn, 2);
+    #pragma omp parallel for
+    for (int pn = 0; pn < total_num_particles_to_recv ; pn++)
+    {
+        float x = rsorted(pn, 0);
+        float y = rsorted(pn, 1);
+        float z = rsorted(pn, 2);
 
-    //        float rx = (x + 0.5) * nGrid; //std::ceil(upperboundary)
-    //        float ry = (y + 0.5) * nGrid;
-    //        float rz = (z + 0.5) * nGrid;
+        float rx = (x + 0.5) * nGrid; //std::ceil(upperboundary)
+        float ry = (y + 0.5) * nGrid;
+        float rz = (z + 0.5) * nGrid;
 
-    //        // precalculate Wx, Wy, Wz and return start index
-    //        float Wx[order], Wy[order], Wz[order];
-    //        int i_start = precalculate_W(Wx, order, rx);
-    //        int j_start = precalculate_W(Wy, order, ry);
-    //        int k_start = precalculate_W(Wz, order, rz);
+        // precalculate Wx, Wy, Wz and return start index
+        float Wx[order], Wy[order], Wz[order];
+        int i_start = precalculate_W(Wx, order, rx);
+        int j_start = precalculate_W(Wy, order, ry);
+        int k_start = precalculate_W(Wz, order, rz);
 
-    //        for (int i = i_start; i < i_start + order; i++)
-    //        {
-    //            for (int j = j_start; j < j_start + order; j++)
-    //            {
-    //                for (int k = k_start; k < k_start + order; k++)
-    //                {
-    //                    float W_res = Wx[i - i_start] * Wy[j - j_start] * Wz[k - k_start];
-    //                    // Deposit the mass onto grid(i,j,k)
-    //                    #pragma omp atomic
-    //                    grid(wrap_edge(i, nGrid), wrap_edge(j, nGrid), wrap_edge(k, nGrid)) += W_res; //std::ceil(upperboundary)
-    //                }
-    //            }
-    //        }
-    //    }
+        for (int i = i_start; i < i_start + order; i++)
+        {
+            for (int j = j_start; j < j_start + order; j++)
+            {
+                for (int k = k_start; k < k_start + order; k++)
+                {
+                    float W_res = Wx[i - i_start] * Wy[j - j_start] * Wz[k - k_start];
+                    // Deposit the mass onto grid(i,j,k)
+                    #pragma omp atomic
+                    grid(wrap_edge(i, nGrid), wrap_edge(j, nGrid), wrap_edge(k, nGrid)) += W_res; //std::ceil(upperboundary)
+                }
+            }
+        }
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
