9e9a5e537423590ad7d9e236f3e967329d9c2542
TaylanSoydan <taylansoydan@gmail.com>
2023-05-14 14:10:10 +0200

    9

---
+++ exercise_session_09/plot_binning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+FILENAME = 'variable_binning'
+
+
+def log(x):
+    if x == 0:
+        return 0
+    else:
+        return np.log10(x)
+
+data = pd.read_csv(f'{FILENAME}.csv')
+plt.plot(data['k'].apply(log), data['P_k'].apply(log))
+plt.xlabel('log(k)')
+plt.ylabel('log(P(k))')
+plt.savefig(f'{FILENAME}.jpg')
+plt.show()
